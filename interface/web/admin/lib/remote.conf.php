ee8e8f4f344fa76764092cfdbc59cc859f744c74
Jesse Norell <jesse@kci.net>
2022-03-21 11:58:32 -0600

    add acme proxy functions to remote api

--- interface/web/admin/lib/remote.conf.php
+++ interface/web/admin/lib/remote.conf.php
@@ -8,4 +8,6 @@ $function_list['admin_record_permissions'] = 'Record permission changes';
 #
 $function_list['client_get_id,login,logout,mail_alias_get,mail_fetchmail_add,mail_fetchmail_delete,mail_fetchmail_get,mail_fetchmail_update,mail_policy_get,mail_spamfilter_blacklist_add,mail_spamfilter_blacklist_delete,mail_spamfilter_blacklist_get,mail_spamfilter_blacklist_update,mail_spamfilter_user_add,mail_spamfilter_user_get,mail_spamfilter_user_update,mail_spamfilter_whitelist_add,mail_spamfilter_whitelist_delete,mail_spamfilter_whitelist_get,mail_spamfilter_whitelist_update,mail_user_filter_add,mail_user_filter_delete,mail_user_filter_get,mail_user_filter_update,mail_user_get,mail_user_update,server_get,server_get_app_version'] = 'Roundcube plugins functions';
 
-?>
+# functions used by ispc_acmeproxy:  https://git.ispconfig.org/ispconfig/Modules/-/tree/master/ispc_acmeproxy
+#
+$function_list['client_get_by_username,client_get_id,dns_zone_get,dns_zone_get_by_user,dns_zone_update,dns_txt_add,dns_txt_get,dns_txt_delete'] = 'ISPC acme proxy functions';
